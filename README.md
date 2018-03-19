44846b8bcb0b78e746821109c2d850e584558595
Everton Morais <esdmorais@gmail.com>
2018-03-18 21:01:06 -0300

    some adjusts and refactoring in order service

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
+# README #
+
 # GetYourFood
 SkipTheDishes São Paulo Recruiting Fair
+
+### Running tests ###
+* Open the Terminal
+* Go to the location of the project
+* Run the command: 
+
+```
+#!cmd
+
+mvn test
+```
+
+### Running the application ###
+* Open the Terminal
+* Go to the location of the project
+* Run the command: 
+```
+#!cmd
+
+mvn spring-boot:run
+```
+
+### Endpoints ###
+* [http://localhost:8080/api/v1/swagger-ui.html](Link URL)
+
+### H2 Database Console ###
+* On the browser, access: [http://localhost:8080/api/v1/h2-console/](Link URL)
+* Fill the field JDBC URL with: **jdbc:h2:mem:testdb**
+* Press **Connect** button
+
+### Notes ###
+* I used TDD to develop most of parts. Almost all the relevant services and repositories are tested;
+* I splitted my project into pieces to help me to organize what shoud I do first, ordering by relevance:
+
+- create project using spring initializr OK
+- configure swagger OK
+- run at least one time to see if it's working OK
+
+Cousine OK!
+01) GET /api/v1/Cousine OK
+02) GET /api/v1/Cousine/search/{searchText} OK
+06) GET /api/v1/Cousine/{cousineId}/stores OK
+
+Store OK!
+03) GET /api/v1/Store OK
+04) GET /api/v1/Store/search/{searchText} OK
+05) GET /api/v1/Store/{storeId} OK
+10) GET /api/v1/Store/{storeId}/products OK
+
+Product OK!
+07) GET /api/v1/Product OK
+08) GET /api/v1/Product/search/{searchText} OK
+09) GET /api/v1/Product/{productId} OK
+
+Order
+13) GET /api/v1/Order/{orderId} OK (not tested)
+12) POST /api/v1/Order (products are not persisting correctly and there is not test for prices and totals)
+15) GET /api/v1/Order/customer 
+
+Customer
+14) POST /api/v1/Customer/auth (100 minutes!)
+11) POST /api/v1/Customer OK (need to response the auth token)
+
+
+
--- src/main/java/br/com/getyourfood/order/OrderController.java
+++ src/main/java/br/com/getyourfood/order/OrderController.java
@@ -13,24 +13,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.getyourfood.product.Product;
+import br.com.getyourfood.product.ProductRepository;
+import br.com.getyourfood.store.Store;
+import br.com.getyourfood.store.StoreRepository;
+
 @RestController
 @RequestMapping(value = "/Order", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
 
     private OrderRepository orderRepository;
+    private StoreRepository storeRepository;
+    private ProductRepository productRepository;
 
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, StoreRepository storeRepository, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
+        this.storeRepository = storeRepository;
+        this.productRepository = productRepository;
     }
 
     @PostMapping
     @Transactional
     public Order createNewOrder(@Valid @RequestBody Order order) {
-        if (order.getId() > 0) {
-            // TODO change to a better solution using java8
-            order.setLastUpdate(new Date());
-        }
+        refreshOrder(order);
         return orderRepository.save(order);
+    }
+
+    private void refreshOrder(Order order) {
+        // TODO change to a better solution using java8, or JodaTime
+        order.setLastUpdate(new Date());
+
+        Store selectedStore = storeRepository.findById(order.getStoreId()).get();
+        order.setStore(selectedStore);
+
+        refreshProducts(order);
+    }
+
+    private void refreshProducts(Order order) {
+        order.getOrderItems().forEach(orderItem -> {
+            Product selectedProduct = productRepository.findById(orderItem.getProductId()).get();
+            orderItem.setProduct(selectedProduct);
+            orderItem.setPrice(orderItem.getProduct().getPrice());
+            orderItem.setTotal(orderItem.getPrice() * orderItem.getQuantity());
+        });
+
+        double total = order.getOrderItems().stream().mapToDouble(orderItem -> orderItem.getTotal()).sum();
+        order.setTotal(total);
     }
 
     @GetMapping("/{orderId}")
