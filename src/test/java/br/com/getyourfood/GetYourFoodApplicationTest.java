678f6853d2648c6f6baf61f6567af695258b8cc9
Everton Morais <esdmorais@gmail.com>
2018-03-27 21:29:05 -0300

    improvments to persist inicial cusine, store and product data

--- src/test/java/br/com/getyourfood/GetYourFoodApplicationTest.java
+++ src/test/java/br/com/getyourfood/GetYourFoodApplicationTest.java
@@ -1,9 +1,22 @@
 package br.com.getyourfood;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.getyourfood.cousine.Cousine;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +26,13 @@ public class GetYourFoodApplicationTest {
     public void contextLoads() {
     }
 
+    @Test
+    public void parseJsonCollectionToObjectFromFile() throws JsonParseException, JsonMappingException, IOException {
+        File cousineJsonFile = new File("./src/main/resources/static/cousine.json");
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<Cousine> cousines = mapper.readValue(cousineJsonFile, new TypeReference<List<Cousine>>() {});
+
+        assertEquals(4, cousines.size());
+    }
 }
